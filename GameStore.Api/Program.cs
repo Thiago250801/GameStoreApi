ccdbe37a30aef79b80de24efa193bfde3a6a3205
Thiago Arthur <thlimabsgim@gmail.com>
2024-07-19 12:09:49 -0400

    Gitignore criado através do comando do terminal : dotnet new gitgnore

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -5,10 +5,10 @@ var app = builder.Build();
 
 
 List<GameDto> games = [
-     new (1, "EA 24", "Sports", 259.00M, new DateOnly(2028,09,29)),
-     new (2, "Resident Evil 4 Remake", "Action, Adventure", 169.00M, new DateOnly(2023,03,23)),
-     new (3, "Counter Strike 2","FPS", 00M, new DateOnly(2024,01,15)),
-     new (4, "EA 24", "Sports", 255.23M, new DateOnly(2024,01,15)),
+     new (1, "EA 24", "Sports", 259.00M, new DateOnly(2023,09,29)),
+     new (2, "Resident Evil 4 Remake", "Ação, Aventura", 169.00M, new DateOnly(2023,03,23)),
+     new (3, "Counter Strike 2","FPS", 00M, new DateOnly(2012,08,21)),
+     new (4, "Red Dead Redemption 2", "Ação, Aventura", 98.96M, new DateOnly(2019,12,05)),
 ];
 
 app.MapGet("games", () => games);

3ff9a28aef4b662b307e58724e7b2fcbb2af2582
Thiago Arthur <thlimabsgim@gmail.com>
2024-07-19 11:49:56 -0400

    Criado o GameDTO e Criado uma lista de games para teste de rota

--- GameStore.Api/Dtos/GameDto.cs
+++ GameStore.Api/Dtos/GameDto.cs
@@ -1,6 +1,12 @@
-﻿namespace GameStore.Api;
+﻿namespace GameStore.Api.Dtos;
 
-public record class GameDto
-{
-
+public record class GameDto(
+    int id,
+    string Name,
+    string Genre,
+    decimal Price,
+    DateOnly ReleaseDate)
+{   
+    // transformando o preço em String o currency pt-br
+    public string  PriceDisplay => Price.ToString("C");
 }
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,5 +1,17 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+
+List<GameDto> games = [
+     new (1, "EA 24", "Sports", 259.00M, new DateOnly(2028,09,29)),
+     new (2, "Resident Evil 4 Remake", "Action, Adventure", 169.00M, new DateOnly(2023,03,23)),
+     new (3, "Counter Strike 2","FPS", 00M, new DateOnly(2024,01,15)),
+     new (4, "EA 24", "Sports", 255.23M, new DateOnly(2024,01,15)),
+];
+
+app.MapGet("games", () => games);
 
 app.MapGet("/", () => "Hello World!");
 

6600d799eb669b6c098c661a266a71c85b042e40
Thiago Arthur <thlimabsgim@gmail.com>
2024-07-19 12:30:45 -0400

    test ignore

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -8,7 +8,7 @@ List<GameDto> games = [
      new (1, "EA 24", "Sports", 259.00M, new DateOnly(2023,09,29)),
      new (2, "Resident Evil 4 Remake", "Ação, Aventura", 169.00M, new DateOnly(2023,03,23)),
      new (3, "Counter Strike 2","FPS", 00M, new DateOnly(2012,08,21)),
-     new (5, "Red Dead Redemption 2", "Ação, Aventura", 98.96M, new DateOnly(2019,12,05)),
+     new (4, "Red Dead Redemption 2", "Ação, Aventura", 98.96M, new DateOnly(2019,12,05)),
 ];
 
 app.MapGet("games", () => games);
